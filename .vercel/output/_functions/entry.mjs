a08659ee6ab8408c798a6646a5a3e688b8baf492
vkaidun <vkaidun@gmail.com>
2026-06-28 15:26:41 +0200

    Fix: prerender index page so YAML content loads correctly on Vercel

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_k_tjFyZh.mjs';
-import { manifest } from './manifest_Chm3EnVn.mjs';
+import { manifest } from './manifest_Cain4VBF.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "9965a7ca-f70a-4f97-b354-2eade7052895",
+    "middlewareSecret": "a426b815-dab9-4577-9a71-cc9651aee214",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
